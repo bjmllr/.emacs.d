bd37937b54dc37f01bbd8268ef596ba067a1f439
Ben Miller <bjmllr@gmail.com>
2015-09-17 11:06:27 -0400

    add eval-and-replace for emacs lisp

--- init.el
+++ init.el
@@ -251,6 +251,17 @@
 (add-hook 'emacs-lisp-mode 'whitespace-mode)
 (add-hook 'emacs-lisp-mode 'aggressive-indent-mode)
 
+;; http://emacsredux.com/blog/2013/06/21/eval-and-replace/
+(defun eval-and-replace ()
+  "Replace the preceding sexp with its value."
+  (interactive)
+  (backward-kill-sexp)
+  (condition-case nil
+      (prin1 (eval (read (current-kill 0)))
+             (current-buffer))
+    (error (message "Invalid expression")
+           (insert (current-kill 0)))))
+
 ;;;;; HTML
 (add-hook 'html-mode-hook (lambda () (setq indent-tabs-mode nil)))
 (add-hook 'html-mode-hook 'whitespace-mode)
